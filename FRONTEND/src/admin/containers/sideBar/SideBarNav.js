45024873583b2d964136712a8ff7481980b726f2
Deadeyeee <prago.jhimwel@ue.edu.ph>
2022-04-04 23:32:41 +0800

    2

---
+++ FRONTEND/src/admin/containers/sideBar/SideBarNav.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import logo from '../../../client/images/logo.png'
+import MenuBookIcon from '@mui/icons-material/MenuBook';
+import AccountBalanceWalletIcon from '@mui/icons-material/AccountBalanceWallet';
+import PeopleIcon from '@mui/icons-material/People';
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import BookIcon from '@mui/icons-material/Book';
+import ContentPasteIcon from '@mui/icons-material/ContentPaste';
+import SensorDoorIcon from '@mui/icons-material/SensorDoor';
+import MeetingRoomIcon from '@mui/icons-material/MeetingRoom';
+import MailIcon from '@mui/icons-material/Mail';
+import AssessmentIcon from '@mui/icons-material/Assessment';
+
+const SideBarNav = () => {
+  return (
+    <div>SideBarNav</div>
+  )
+}
+
+export default SideBarNav
---
+++ FRONTEND/src/admin/pages/profile/Profile.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import SideBarNav from '../../containers/sideBar/SideBarNav'
+import { Container } from './style'
+
+const profile = () => {
+  return (
+    <Container>
+        <SideBarNav>
+
+        </SideBarNav>
+    </Container>
+  )
+}
+
+export default profile
