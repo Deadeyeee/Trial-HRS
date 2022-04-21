4e261060239ac20cfec0bde9911b1e08dd367874
meelvore <delacruz.rasheed@ue.edu.ph>
2022-04-22 06:41:29 +0800

    bold-part2

--- FRONTEND/src/admin/containers/roomDetailsContainer/RoomDetailsContainer.js
+++ FRONTEND/src/admin/containers/roomDetailsContainer/RoomDetailsContainer.js
@@ -4,87 +4,234 @@ import { HorizontalLine } from '../../../client/components/horizontalLine/Horizo
 import { Title } from '../../../client/components/title/styles'
 import { ContainerGlobal } from '../../components/container/container'
 import { Container, HeadContainer, TableContainer, Td, Th, Tr } from './style'
+import Modal from '@mui/material/Modal';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import { InputContainer } from '../../../client/containers/informationForm/style';
+import TextField from '@mui/material/TextField';
+import { styled } from '@mui/material/styles';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+import IconButton from '@mui/material/IconButton';
+import PhotoCamera from '@mui/icons-material/PhotoCamera';
+import delux from '../../../client/images/RoomsIMG/delux.jpg';
+import prm from '../../../client/images/RoomsIMG/premium.jpg';
+
+const style = {
+    position: 'absolute',
+    display: 'flex',
+    top: '50%',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center ',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    height: '80vh',
+    overflow: 'hidden',
+    width: 'auto',
+    bgcolor: 'background.paper',
+    boxShadow: 24,
+    p: 4,
+    radius: '.5rem',
+};
+
+const Input = styled('input')({
+    display: 'none',
+});
+
+const itemData = [
+    {
+        img: delux,
+    },
+    {
+        img: prm,
+    },
+    {
+        img: '',
+    },
+    {
+        img: '',
+    },
+    {
+        img: '',
+    },
+    {
+        img: '',
+    },
+    {
+        img: '',
+    },
+    {
+        img: '',
+    },
+    {
+        img: '',
+    },
+];
 
 const RoomDetailsContainer = () => {
-  return (
-    <Container>
-        <HeadContainer>
-        <Title
-                size='20px'
-                color='white'
-                family='Helvetica'
-                fstyle='normal'
-                weight='600'
-                align='left'
-                margin='20px 0px 20px 30px'
+
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
+    return (
+
+        <Container>
+            <HeadContainer>
+                <Title
+                    size='20px'
+                    color='white'
+                    family='Helvetica'
+                    fstyle='normal'
+                    weight='600'
+                    align='left'
+                    margin='20px 0px 20px 30px'
+                >
+                    Room Details
+                </Title>
+            </HeadContainer>
+            <ContainerGlobal
+                whileHover={{ scale: 1.01 }}
+                whileTap={{ scale: 1 }}
+                w='90%'
+                h='auto'
+                bg='white'
+                direction='column'
+                padding='30px'
+                margin='20px 0px 50px 0px'
+                gap='10px'
             >
-            Room Details
-        </Title>
-        </HeadContainer>
-        <ContainerGlobal
-            whileHover={{ scale: 1.01 }}
-            whileTap={{ scale: 1 }}
-            w='90%'
-            h='auto'
-            bg='white'
-            direction='column'
-            padding='30px'
-            margin='20px 0px 50px 0px'
-            gap='10px'
-        >
-        <Title
-            size='26px'
-            color='black'
-            family='Helvetica'
-            fstyle='normal'
-            weight='600'
-            align='left'
-        >
-        Room Details
-        </Title>
-        <HorizontalLine
-            bg='gray'
-            w='100%'
-            margin='0px'
-         >
-        </HorizontalLine>
-        <TableContainer>
-        <Tr>
-            <Th align='center'>Room Type</Th>
-            <Th align='center'>Weekday Rate</Th>
-            <Th align='center'>Weekend Rate</Th>
-            <Th align='center'>Addtional Service</Th>
-            <Th align='center'>Action</Th>
-        </Tr>
-        <Tr>
-            <Td align='center'>Family Room</Td>
-            <Td align='center'>PHP 2,500</Td>
-            <Td align='center'>PHP 2,700</Td>
-            <Td align='center'>.........</Td>
-            <Td align='center'>...</Td>
-        </Tr>
-        <Tr>
-            <Td align='center'>Premium Room</Td>
-            <Td align='center'>PHP 3,500</Td>
-            <Td align='center'>PHP 3,700</Td>
-            <Td align='center'>.........</Td>
-            <Td align='center'>...</Td>
-        </Tr>
-        <Tr>
-            <Td align='center'>Deluxe Room</Td>
-            <Td align='center'>PHP 5,000</Td>
-            <Td align='center'>PHP 5,200</Td>
-            <Td align='center'>.........</Td>
-            <Td align='center'>...</Td>
-        </Tr>
-        </TableContainer>
-        </ContainerGlobal>
-        <Button variant="contained" size="large"
+                <Title
+                    size='26px'
+                    color='black'
+                    family='Helvetica'
+                    fstyle='normal'
+                    weight='600'
+                    align='left'
+                >
+                    Room Details
+                </Title>
+                <HorizontalLine
+                    bg='gray'
+                    w='100%'
+                    margin='0px'
+                >
+                </HorizontalLine>
+                <TableContainer>
+                    <Tr>
+                        <Th align='center'>Room Type</Th>
+                        <Th align='center'>Rate/Night</Th>
+                        <Th align='center'>Action</Th>
+                    </Tr>
+                    <Tr>
+                        <Td align='center'>Family Room</Td>
+                        <Td align='center'>PHP 2,500</Td>
+                        <Td align='center'>...</Td>
+                    </Tr>
+                    <Tr>
+                        <Td align='center'>Premium Room</Td>
+                        <Td align='center'>PHP 3,500</Td>
+                        <Td align='center'>...</Td>
+                    </Tr>
+                    <Tr>
+                        <Td align='center'>Deluxe Room</Td>
+                        <Td align='center'>PHP 5,000</Td>
+                        <Td align='center'>...</Td>
+                    </Tr>
+                </TableContainer>
+            </ContainerGlobal>
+
+            <Button
+                variant="contained"
+                size="large"
+                onClick={handleOpen}
                 style={{ backgroundColor: '#2E2E2E' }}>
                 Add
-        </Button>
-    </Container>
-  )
+            </Button>
+
+            <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <Box sx={style}>
+                    <Typography id="modal-modal-title" variant="h4" component="h2"
+                        style={{ textAlign: 'center', padding: '10px', marginTop: '50px' }}>
+                        Add Room
+                    </Typography>
+
+                    <InputContainer>
+                        <TextField
+                            placeholder='Room Type'
+                            label="Room Type"
+                            variant="outlined"
+                            style={{ width: '55%', }} />
+
+                        <TextField
+                            placeholder='Rate per Night'
+                            label="Rate per Night"
+                            variant="outlined"
+                            style={{ width: '55%', }} />
+                    </InputContainer>
+
+                    <Typography id="modal-modal-title" variant="h5" component="h2"
+                        style={{ textAlign: 'center', margin: '20px 0px 0px 0px', }}>
+                        Upload Photo
+                    </Typography>
+
+                    <InputContainer>
+
+                        <ImageList sx={{ width: '100%', height: 360 }} cols={1} rowHeight={164}>
+                            {itemData.map((item) => (
+                                <ImageListItem
+                                    style={{
+                                        backgroundColor: 'rgba(0, 0, 0, .1)',
+                                        width: 'auto', display: 'flex', justifyContent: 'center',
+                                        alignItems: 'center',
+                                        backgroundImage: `url(${item.img})`,
+                                        backgroundSize: 'cover',
+
+                                    }}>
+
+                                    <label htmlFor="icon-button-file">
+                                        <Input accept="image/*" id="icon-button-file" type="file" />
+                                        <IconButton aria-label="upload picture" component="span"
+                                        style={{ backgroundColor: 'rgba(0, 0, 0, 0.5)', }} >
+                                            <PhotoCamera
+                                                style={{ color: '#CCA041', fontSize: '60px', }} />
+                                        </IconButton>
+                                    </label>
+                                </ImageListItem>
+                            ))}
+                        </ImageList>
+                    </InputContainer>
+
+
+                    <InputContainer>
+
+                        <Button
+                            variant="contained"
+                            size="large"
+                            style={{ backgroundColor: "rgba(219, 51, 51, 1)" }}
+                            onClick={handleClose}>
+                            Cancel
+                        </Button>
+
+                        <Button
+                            variant="contained"
+                            size="large"
+                            style={{ backgroundColor: '#0C4426' }}
+                            onClick={handleClose}>
+                            Add
+                        </Button>
+
+                    </InputContainer>
+                </Box>
+            </Modal>
+        </Container>
+    )
 }
 
 export default RoomDetailsContainer
